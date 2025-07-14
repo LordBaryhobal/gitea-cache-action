ff686dd49e727d724a16ae32d82d13cd372b4d3d
LordBaryhobal <lordbaryhobal@gmail.com>
2025-07-15 00:12:57 +0200

    disable ghes no support check

--- src/utils/actionUtils.ts
+++ src/utils/actionUtils.ts
@@ -4,16 +4,7 @@ import * as core from "@actions/core";
 import { RefKey } from "../constants";
 
 export function isGhes(): boolean {
-    const ghUrl = new URL(
-        process.env["GITHUB_SERVER_URL"] || "https://github.com"
-    );
-
-    const hostname = ghUrl.hostname.trimEnd().toUpperCase();
-    const isGitHubHost = hostname === "GITHUB.COM";
-    const isGitHubEnterpriseCloudHost = hostname.endsWith(".GHE.COM");
-    const isLocalHost = hostname.endsWith(".LOCALHOST");
-
-    return !isGitHubHost && !isGitHubEnterpriseCloudHost && !isLocalHost;
+    return false;
 }
 
 export function isExactKeyMatch(key: string, cacheKey?: string): boolean {
